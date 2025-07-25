9f3c6698e662593debffb9950fd22062c6dfed0c
ralfaro <rodrigo.alfaro@salud.gob.sv>
2025-07-25 15:23:17 -0600

    :sparkles: I added the react hook form

--- src/app/components/ContactForm/ContactForm.tsx
+++ src/app/components/ContactForm/ContactForm.tsx
@@ -1,30 +1,47 @@
+'use client'
 import Image from "next/image"
+import { useForm } from "react-hook-form";
 export const ContactForm = () => {
+
+    type Input = {
+        name:string,
+        lastname:string,
+        age:number,
+        email:string,
+        comment:string
+    }
+
+    const {register, handleSubmit, watch, formState: {errors}} = useForm<Input>();
+
+    const onsubmit = (data:Input) => {
+        console.log(data);
+    }
+
   return (
     <section className="mx-auto mt-10 mb-10 text-white w-full flex justify-center">
     <div className="grid grid-cols-1 md:grid-cols-2 w-full md:w-2/3">
         <div className="flex flex-col justify-center items-center p-2 ">
-            <form action="" className="w-full rounded-lg flex flex-col p-4 shadow-amber-500 shadow-xl/30" style={{background:"linear-gradient(159deg,rgba(71, 68, 39, 1) 0%, rgba(224, 187, 63, 1) 100%)"}}>
+            <form action="" onSubmit={handleSubmit(onsubmit)} className="w-full rounded-lg flex flex-col p-4 shadow-amber-500 shadow-xl/30" style={{background:"linear-gradient(159deg,rgba(71, 68, 39, 1) 0%, rgba(224, 187, 63, 1) 100%)"}}>
                 <div className="flex flex-col py-2">
                     <label htmlFor="name" className="font-bold">Name:</label>
-                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="text" name="name" id="name" placeholder="Insert your full name" />
+                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="text" {...register('name')} id="name" placeholder="Insert your full name" />
                 </div>
                 <div className="flex flex-col py-2">
                     <label htmlFor="name" className="font-bold">Lastname:</label>
-                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="text" name="lastname" id="lastname" placeholder="Insert your full lastname" />
+                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="text" {...register('lastname')} id="lastname" placeholder="Insert your full lastname" />
                 </div>
                 <div className="flex flex-col py-2">
                     <label htmlFor="age" className="font-bold">Age:</label>
-                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="number" name="age" id="age" placeholder="Insert your age" />
+                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="number" {...register('age')}  id="age" placeholder="Insert your age" />
                 </div>
                 <div className="flex flex-col py-2">
                     <label htmlFor="email" className="font-bold">Email:</label>
-                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="email" name="email" id="email" placeholder="Insert your email address" />
+                    <input className="p-2 bg-white rounded-lg placeholder:text-black mt-2" type="email" {...register('email')}  id="email" placeholder="Insert your email address" />
                 </div>
                 <div className="flex flex-col py-2">
                     <label htmlFor="comment" className="font-bold">Comment:</label>
                     <textarea
-                        name="comment"
+                        {...register('comment')}
                         id="comment"
                         placeholder="Insert your comment"
                         rows={4}
